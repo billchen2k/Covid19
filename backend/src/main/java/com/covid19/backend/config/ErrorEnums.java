4380ff381904f6dd8ab3288f0fef7963995da9a5
Bill Chen <Bill.Chen@live.com>
2020-06-10 10:19:48 +0800

    全局错误代码处理

---
+++ backend/src/main/java/com/covid19/backend/config/ErrorEnums.java
@@ -0,0 +1,29 @@
+package com.covid19.backend.config;
+
+import lombok.Getter;
+
+/**
+ * 全局返回代码类
+ *
+ * @author billchen
+ * @version 1.0
+ * @create 2020-06-10 10:04
+ **/
+@Getter
+public enum ErrorEnums {
+
+	CODE_SUCCESS(200, "成功"),
+	CODE_ERROR(500, "出现异常"),
+	CODE_VALIDATE_FAILED(405, "验证失败"),
+	CODE_NOT_FOUND(404, "无法找到"),
+	CODE_UNAUTHORIZED(401, "权限不足"),
+	CODE_FORBIDDEN(403, "禁止访问");
+
+	public int code;
+	public String msg;
+
+	ErrorEnums(int code, String msg) {
+		this.code = code;
+		this.msg = msg;
+	}
+}
--- backend/src/main/java/com/covid19/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/covid19/backend/config/SwaggerConfig.java
@@ -3,13 +3,21 @@ package com.covid19.backend.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @ConditionalOnExpression("${swagger.enable:true}") //当enable为true时才选择加载该配置类
@@ -18,13 +26,24 @@ public class SwaggerConfig{
 
 	@Bean
 	public Docket createMainApi() {
+		List<ResponseMessage> responseMessageList = new ArrayList<>();
+		Arrays.stream(ErrorEnums.values()).forEach(errorEnums -> {
+			responseMessageList.add(
+					new ResponseMessageBuilder().code(errorEnums.getCode()).message(errorEnums.getMsg()).responseModel(
+							new ModelRef("string")).build()
+			);
+		});
 		return new Docket(DocumentationType.SWAGGER_2)
 				//.groupName("API")//如果要写多个docket，就需要为它们指定不同groupName
 				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.covid19.backend"))//设定扫描范围
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.globalResponseMessage(RequestMethod.GET, responseMessageList)
+				.globalResponseMessage(RequestMethod.POST, responseMessageList)
+				.globalResponseMessage(RequestMethod.PUT, responseMessageList)
+				.globalResponseMessage(RequestMethod.DELETE, responseMessageList);
 	}
 
 //	@Bean
