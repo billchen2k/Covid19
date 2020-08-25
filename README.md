68c675dec862771f5e2ac1ccf6297df318bb571a
Bill Chen <Bill.Chen@live.com>
2020-08-25 22:01:09 +0800

    Create README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Covid-19 疫情分析与管理系统
+
+*Covid-19 Analysis & Manage System*
+
+> 数据库系统课程 2020 期末项目
+
+Built with vue.js + nuxt.js + SpringBoot.
--- frontend/components/global/Config.vue
+++ frontend/components/global/Config.vue
@@ -7,9 +7,9 @@
   /** Allow Credentials Globally **/
   export default {
     name: 'config',
-    // apiurl: 'http://8.210.248.203'
+    apiurl: 'http://8.210.248.203'
     // apiurl: 'http://45.76.79.85'
-    apiurl: 'http://localhost'
+    // apiurl: 'http://localhost'
   }
 </script>
 
